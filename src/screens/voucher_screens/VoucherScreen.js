f7d0f185dc5d9419037024f5ee04afef5463bc7f
Thang Hoang Ngoc <thanghoangngoc@mmms-MacBook-Pro.local>
2018-11-11 10:20:24 +0700

    thanghn task1

---
+++ src/screens/voucher_screens/VoucherScreen.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+export default class VoucherScreen extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+        };
+    }
+
+    render() {
+        return (
+            <View style={styles.container}>
+                <Text> VoucherScreen </Text>
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1
+    }
+})
